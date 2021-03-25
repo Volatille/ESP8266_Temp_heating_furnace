817b918ae4c429bb2f35302d4e06d45e6b4d6f73
Volatille <popik009@o2.pl>
2021-03-25 20:43:01 +0100

    Sensor software start

---
+++ DS18B20/DS18B20.ino
@@ -0,0 +1,29 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com  
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const int oneWireBus = 2;     
+OneWire sensor(oneWireBus);
+DallasTemperature sensors(&sensor);
+
+void setup() 
+{
+  Serial.begin(115200);
+  sensors.begin();
+}
+
+void loop() {
+   
+    sensors.requestTemperatures(); 
+   float temp_Measurement = sensors.getTempCByIndex(0);
+  
+  Serial.print(temp_Measurement);
+  Serial.println("C");
+ 
+  delay(2000);
+
+}
